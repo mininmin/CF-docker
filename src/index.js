7c01442a876cc11b411eecdbf3591091cd35162a
mininmin <77315105+mininmin@users.noreply.github.com>
2024-06-13 10:49:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.sayes.cc": "https://registry-1.docker.io",
+  "quay.sayes.cc": "https://quay.io",
+  "gcr.sayes.cc": "https://gcr.io",
+  "k8s-gcr.sayes.cc": "https://k8s.gcr.io",
+  "k8s.sayes.cc": "https://registry.k8s.io",
+  "ghcr.sayes.cc": "https://ghcr.io",
+  "cloudsmith.sayes.cc": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
